1d59a36f80abd1e197c7d364770ed91b627709d5
Volkane23 <mimizouta@gmail.com>
2023-12-17 10:54:03 +0100

    update khfif driyf

---
+++ src/main/java/com/example/Project/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.Project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Thymeleaf is working!");
+        return "base";
+    }
+}
