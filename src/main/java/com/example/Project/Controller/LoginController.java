e0749f4462f090a03c7b71f95cb5eaaa6d369a11
abdelilahhanafi <abdelilah.hanafi@uir.ac.ma>
2023-12-25 12:52:59 +0100

    l3ilm

--- src/main/java/com/example/Project/Controller/LoginController.java
+++ src/main/java/com/example/Project/Controller/LoginController.java
@@ -1,11 +1,40 @@
 package com.example.Project.Controller;
 
+import com.example.Project.Model.User;
+import com.example.Project.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginController {
-    //@Override
-    public String login() {
+
+    @Autowired
+    private UserDao userDao;
 
 
-
+    public LoginController(UserDao userDao){
+        this.userDao=userDao;
+    }
+    @GetMapping("/login")
+    public String getLoginPage(@ModelAttribute("user") User user) {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("user") User user, Model model) {
+        User foundUser = userDao.findByEmail(user.getEmail());
+
+        if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+
+            return "redirect:profile.html";
+        } else {
+
+            model.addAttribute("message", "Invalid username or password");
+            return "login";
+        }
     }
 }
