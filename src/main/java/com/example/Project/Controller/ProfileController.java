8fab1a5545ca15ae7773ca4aa5d485cc4ca642ee
MrQuack23 <90573610+MrQuack23@users.noreply.github.com>
2023-12-25 16:31:25 +0100

    update malaki hhh, nsit chno 9sst

--- src/main/java/com/example/Project/Controller/ProfileController.java
+++ src/main/java/com/example/Project/Controller/ProfileController.java
@@ -1,15 +1,40 @@
 package com.example.Project.Controller;
 
+import com.example.Project.Model.Recipe;
 import com.example.Project.Model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.Project.Service.UserService;
+import com.example.Project.dao.RecipeDao;
+import com.example.Project.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ProfileController {
 
-    @RequestMapping("/profile")
-    public String showFrom(Model model){
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/profile")
+    public String showprofile(Model model){
+        return "addrecipe";
+    }
+
+    @PostMapping("/profile")
+    public String updateUser(@ModelAttribute("user") User user, Model model){
+        User existingUser = UserService.findById();
+
+        if(existingUser != null){
+            existingUser.setName(user.getName());
+            existingUser.setPassword(user.getPassword());
+            userDao.save(existingUser);
+            model.addAttribute("message", "Updated successfully");
+        }
 
         return "profile";
     }
