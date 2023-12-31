0d66d5cfbaa77f43ac99a00ed46398f8b1b1b336
abdelilahhanafi <abdelilah.hanafi@uir.ac.ma>
2023-12-31 12:05:42 +0100

    7yedt l controller dial login o zedt l html dial allrecipies

--- src/main/java/com/example/Project/Controller/UserController.java
+++ src/main/java/com/example/Project/Controller/UserController.java
@@ -1,10 +1,39 @@
 package com.example.Project.Controller;
 
+import com.example.Project.Model.User;
+import com.example.Project.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserDao userDao;
 
+
+    public UserController(UserDao userDao){
+        this.userDao=userDao;
+    }
+    @GetMapping("/login")
+    public String getLoginPage(@ModelAttribute("user") User user) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("user") User user, Model model) {
+        User foundUser = userDao.findByEmail(user.getEmail());
+
+        if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+
+            return "home";
+        } else {
+
+            model.addAttribute("message", "Invalid username or password");
+            return "login";
+        }
+    }
 }
