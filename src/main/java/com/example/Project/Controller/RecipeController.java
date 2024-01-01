8c7f6875a6b930de590d6fac6ba82fb2afc230f0
MrQuack23 <90573610+MrQuack23@users.noreply.github.com>
2024-01-01 11:10:47 +0100

    functions of addRecipe in recipe and some funtions of profile (not done yet) in UserController

--- src/main/java/com/example/Project/Controller/RecipeController.java
+++ src/main/java/com/example/Project/Controller/RecipeController.java
@@ -1,10 +1,46 @@
 package com.example.Project.Controller;
 
+import com.example.Project.Model.Recipe;
+import com.example.Project.dao.RecipeDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class RecipeController {
+
+
+    private RecipeDao recipeDao;
+
+    @Autowired
+    public RecipeController(RecipeDao recipeDao){
+        this.recipeDao=recipeDao;
+    }
+
+
+    @GetMapping("/addrecipe")
+    public String showAddRecipe(@ModelAttribute("recipe") Recipe recipe){
+        return "addrecipe";
+    }
+
+
+    @PostMapping("/addrecipe")
+    public String addnewRecipe(@ModelAttribute("recipe") Recipe recipe, Model model){
+        recipeDao.save(recipe);
+        model.addAttribute("message", "Created successfully");
+        return "addrecipe";
+    }
+
+    @PostMapping("/profile")
+    public String listUserRecipes(Model model) {
+        List<Recipe> recipes = recipeDao.findAll();
+        model.addAttribute("recipes", recipes);
+        return "profile";
+    }
 
 }
--- src/main/java/com/example/Project/Controller/UserController.java
+++ src/main/java/com/example/Project/Controller/UserController.java
@@ -1,10 +1,52 @@
 package com.example.Project.Controller;
 
+import com.example.Project.Model.Recipe;
+import com.example.Project.Model.User;
+import com.example.Project.Service.IRecipeService;
+import com.example.Project.Service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
+
+    private IUserService userDao;
+
+    @Autowired
+    public UserController(IUserService userDao) {
+        this.userDao = userDao;
+    }
+
+    /*
+    @GetMapping("/profile")
+    public String showprofile(Model model, @ModelAttribute("user")User user) {
+        User existingUser = userDao.findById(user.getIdUser());
+        model.addAttribute("fullName", existingUser.getName());
+        model.addAttribute("email", existingUser.getEmail());
+        model.addAttribute("password", existingUser.getPassword());
+        return "addrecipe";
+    }
+    */
+
+    @PostMapping("/profile")
+    public User updateUser(@ModelAttribute("user") User user, Model model) {
+        //still needs some changes like the getId method,
+        User existingUser = userDao.findById(user.getIdUser());
+        if (existingUser != null) {
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getPassword());
+            return userDao.save(existingUser);
+        }
+        else{
+            return null;
+        }
+
+    }
 
 }
