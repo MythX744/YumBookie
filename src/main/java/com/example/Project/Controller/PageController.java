24b8eb621ebb903de9f66665017a2a951ecc13b6
Volkane23 <mimizouta@gmail.com>
2024-01-06 12:51:52 +0100

    Wa7d l baraka

---
+++ src/main/java/com/example/Project/Controller/PageController.java
@@ -0,0 +1,43 @@
+package com.example.Project.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/navigation")
+public class PageController {
+
+    @GetMapping("/home")
+    public String LoadHome(Model theModel){
+        return "home";
+    }
+
+    @GetMapping("/lunch")
+    public String LoadLunch(Model theModel){
+        return "";
+    }
+
+    @GetMapping("/breakfast")
+    public String LoadBreakfast(Model theModel){
+        return "";
+    }
+
+    @GetMapping("/snack")
+    public String LoadSnake(Model theModel){
+        return "";
+    }
+
+    @GetMapping("/dinner")
+    public String LoadDinner(Model theModel){
+        return "";
+    }
+
+    @GetMapping("/profile")
+    public String LoadProfile(Model theModel){
+        return "profile";
+    }
+
+
+
+}
