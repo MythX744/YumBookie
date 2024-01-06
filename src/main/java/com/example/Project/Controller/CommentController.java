163b7b28bbe66bbc004eae904fbd9cf46ec17273
Volkane23 <mimizouta@gmail.com>
2024-01-06 10:53:08 +0100

    fixed errors

--- src/main/java/com/example/Project/Controller/CommentController.java
+++ src/main/java/com/example/Project/Controller/CommentController.java
@@ -3,8 +3,10 @@ package com.example.Project.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/comment")
 public class CommentController {
 
 }
--- src/main/java/com/example/Project/Controller/UserController.java
+++ src/main/java/com/example/Project/Controller/UserController.java
@@ -15,13 +15,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 
 @Controller
+@RequestMapping("/User")
 public class UserController {
+
     @Autowired
     private UserDao userDao;
 
